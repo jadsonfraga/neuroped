49f1926d60281c0bd16c74953d7497adb446180d
jadsonfraga <jadsonfraga@hotmail.com>
2026-05-06 20:23:38 -0300

    chore: add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.VITE_APP_BASE_PATH ?? '/neuroped/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    emptyOutDir: true
+  }
+});
