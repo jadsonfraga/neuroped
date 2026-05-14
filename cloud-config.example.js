7e24a1579d0eceae58510736b3771826c08ae09f
jadsonfraga <jadsonfraga@hotmail.com>
2026-05-14 02:27:55 -0300

    feat: adicionar exemplo de configuracao de nuvem

---
+++ cloud-config.example.js
@@ -0,0 +1,7 @@
+window.NEUROPED_CLOUD = {
+  provider: 'supabase',
+  supabaseUrl: 'https://SEU-PROJETO.supabase.co',
+  supabasePublicAnon: 'COLE_AQUI_A_CHAVE_PUBLICA_ANON',
+  enabled: false,
+  projectLabel: 'NeuroPed EDJ Cloud'
+};
