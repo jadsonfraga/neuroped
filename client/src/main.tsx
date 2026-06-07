ab83e16481ec5e0870e85368dc774b305e0cce58
jadsonfraga <jadsonfraga@hotmail.com>
2026-06-07 13:23:06 -0300

    feat(core): aplica PasswordGate global antes do app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { PasswordGate } from "@/components/PasswordGate";
 import "./index.css";
 
 try {
@@ -15,4 +16,8 @@ if (!window.location.hash) {
   window.location.hash = "#/";
 }
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <PasswordGate>
+    <App />
+  </PasswordGate>,
+);
